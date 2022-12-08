39921813faabf2868fa664b8bf558989511a3cca
Arfaa Rashid <arfaamrashid@gmail.com>
2022-12-08 15:40:17 -0500

    Resolved err, Updated notes/comments
    Resolved err with tidyverse by reinstalling dev version
    
    Clarified notes

--- Functional-Analysis/functional_analysis.R
+++ Functional-Analysis/functional_analysis.R
@@ -1,39 +1,42 @@
 
 #Arfaa Rashid
-#Nov 30, 2022
+#Nov 30, 2022 
 #functional_analysis.r
-
+#https://hbctraining.github.io/Training-modules/DGE-functional-analysis/
+  
+#Program takes results of differential analysis on a gene set and performs functional analysis on it using over-representation analysis and functional class scoring methods. Visualizes results of FA using various graphs
 
 #---------------------------
 #Install packages
-#install.packages(c("BiocManager", "devtools"))    #tidyverse already installed
+
+#install.packages(c("BiocManager", "devtools"))    
 #BiocManager::install(c("clusterProfiler", "DOSE", "org.Hs.eg.db", "pathview", "AnnotationDbi", "EnsDb.Hsapiens.v75"))
+#devtools::install_github("tidyverse/tidyverse")
 
 
 #---------------------------
 #Load libraries
-library(ggplot2)
-library(ggrepel)
-library(tidyverse)
+
+library(tidyverse)              #collection of integrated packages for common data science functionalities, eg tibble
+library(org.Hs.eg.db)           #package to query annotation db - gene feature info for particular organism, but only latest genomic build available 
+library(EnsDb.Hsapiens.v75)     #package to query annotation db - get transcript/gene info using gene ID; can specify version for correct genomic build
 library(clusterProfiler)        #performs OR analysis w/ hypergeometric testing
+library(ggrepel)                #for visualisation - text label geom for ggplot to repel labels & prevent overlaps
 library(DOSE)
-library(pathview)  #used for visualization
+library(pathview)               #used for visualisation
 library(AnnotationDbi)
-library(org.Hs.eg.db)           #package links to the relevant dbs for human organisms
-library(EnsDb.Hsapiens.v75)     #package with dbs for correct build - must install correct release of Ensembl to query?
 
 
 #---------------------------
-#Read in data - Bulk RNA-seq data
-#https://hbctraining.github.io/Training-modules/DGE-functional-analysis/lessons/01_setting_up.html - link to data
+#Read in given datafiles -> data is a list of genes - link to data: https://hbctraining.github.io/Training-modules/DGE-functional-analysis/lessons/01_setting_up.html
+
 res_tableOE <- read.csv("data/Mov10oe_DE_results.csv", row.names = 1)   
 #differential expression results
-                                                                        
-#w/out row.names=1, first col is row names instead. parameter sets which col to take row names from
+                                                        
 annotations_ahb <- read.csv("data/annotations_ahb.csv")
 
 #Creates tibble (modified data frame)
-res_tableOE_tb <- res_tableOE %>%                                       # %>% - fancy syntax. equivalent is as_tibble(rownames_to_column(res_tableOE, var="gene"))
+res_tableOE_tb <- res_tableOE %>%
  rownames_to_column(var="gene") %>% 
  as_tibble()
 
@@ -42,23 +45,23 @@ res_tableOE_tb <- res_tableOE %>%                                       # %>% - 
 #Extract annotations from db -> get info about each gene in res_tableOE_tb
 
 
-#org.Hs.eg.db
+#using org.Hs.eg.db
 
-#Return the Ensembl IDs for a set of genes  [returns list(table) of wanted info]
-annotations_orgDb <- AnnotationDbi::select(org.Hs.eg.db, # database
-                                           keys = res_tableOE_tb$gene,  # data to use for retrieval                                 [want info about genes in gene col of our tibble]
-                                           columns = c("ENSEMBL", "ENTREZID","GENENAME"), # information to retrieve for given data  [specific info wanted]
-                                           keytype = "SYMBOL") # type of data given in 'keys' argument                              [? not really sure. heading for keys col(what we gave) in returned table? use keytypes() to find..]
+#Return the Ensembl IDs for a set of genes - get gene names from res_tableOE and query database for their associated ENSEMBL IDs, ENTREZ IDs
+annotations_orgDb <- AnnotationDbi::select(org.Hs.eg.db,
+                                           keys = res_tableOE_tb$gene,                      #data used to query                         
+                                           columns = c("ENSEMBL", "ENTREZID","GENENAME"),   #what we are looking for
+                                           keytype = "SYMBOL")                             
 
-#sometimes duplicates are returned - will keep just first versions
-non_duplicates_idx <- which(duplicated(annotations_orgDb$SYMBOL) == FALSE)    #indices for non dups
+#remove duplicates by keeping only first occurrences 
+non_duplicates_idx <- which(duplicated(annotations_orgDb$SYMBOL) == FALSE)
 annotations_orgDb <- annotations_orgDb[non_duplicates_idx, ]
 
 #some genes have NA - no annotation
-# this is because we used a database for a different gene build than our dataset was based on, so the above annotations are incorrect.. 
+#this is because we used a database for a different genomic build than our dataset was based on, so the above annotations are incorrect
+#instead, use EnsDb.Hsapiens.v75 to get annotations for correct genomic build
 
-
-#EnsDb.Hsapiens.v75
+#using EnsDb.Hsapiens.v75
 
 # Return the Ensembl IDs for a set of genes
 annotations_edb <- AnnotationDbi::select(EnsDb.Hsapiens.v75,
@@ -70,26 +73,29 @@ annotations_edb <- AnnotationDbi::select(EnsDb.Hsapiens.v75,
 non_duplicates_idx <- which(duplicated(annotations_edb$SYMBOL) == FALSE)
 annotations_edb <- annotations_edb[non_duplicates_idx, ]
 
-#no longer any NA entries, so it worked right :>
+#no longer any NA entries, so it worked correctly
 length(which(is.na(annotations_edb$GENEID)))
 
-
+#***what is the point of all this ^ ? never used?
+#*annotations_edb contains mostly same info as annotations_ahb, which workshop says to read in and use instead
+#*seems to have more info
 #---------------------------
 #OR Analysis
 
+#goal: determine which categories are overRep'd in a subset of under/over expressed genes*
+
 ## Merge annotation df with initial results df
-res_ids <- inner_join(res_tableOE_tb, annotations_ahb, by=c("gene"="gene_name"))      #list with cols from orig df and annotations[categories ?]
+res_ids <- inner_join(res_tableOE_tb, annotations_ahb, by=c("gene"="gene_name"))
 
 ## background set: all genes tested for significance in results           
 allOE_genes <- as.character(res_ids$gene_id)    
 
-## significant set: only genes with p-adjusted values<0.05 -> ? we will determine which categories are overRep'd in a subset of under/over expressed genes?
+## significant set: only genes with p-adjusted values<0.05 - ones with a significant difference [in ? geneexp?]
 sigOE <- dplyr::filter(res_ids, padj < 0.05)
-
 sigOE_genes <- as.character(sigOE$gene_id)
 
 ## Gene Ontology enrichment analysis 
-#gene is sig set, universe is background set; uses ensembl IDs to search the org.Hs.eg.db database. *does gene build no longer matter?
+#uses ensembl IDs to search the org.Hs.eg.db database, categorize by GO term, and perform over-rep analysis
 ego <- enrichGO(gene = sigOE_genes, 
                 universe = allOE_genes,
                 keyType = "ENSEMBL",
@@ -99,7 +105,7 @@ ego <- enrichGO(gene = sigOE_genes,
                 qvalueCutoff = 0.05, 
                 readable = TRUE)
 
-cluster_summary <- data.frame(ego) #what does this mean *? what are cols 5-7?
+cluster_summary <- data.frame(ego) 
 save(ego, file="results/ego.rda")
 write.csv(cluster_summary,"results/clusterProfiler_Mov10oe.csv")
 
@@ -109,17 +115,16 @@ write.csv(cluster_summary,"results/clusterProfiler_Mov10oe.csv")
 
 pdf("results/plots.pdf")    #open pdf file to write to 
 
-#dotplot
-dotplot(ego, showCategory=50, title = "OR Analysis")      #* how can i find parameter names to manipulate graph..? args() returning ...
+# Creating Dotplot
+dotplot(ego, showCategory=50, title = "OR Analysis")      #*** how can i find parameter names to manipulate graph..? args() returning ...
 
 
-#netplot
-## To color genes by log2 fold changes, we need to extract the log2 fold changes from our results table creating a named vector
-# (fold change: measurement for change b/w A, B. fold change of B WRT A = B/A)
+# Creating Netplot
+## extracting the log2 fold changes from results table by creating named vector, in order to colour graph by log2fold change
 OE_foldchanges <- sigOE$log2FoldChange
 names(OE_foldchanges) <- sigOE$gene
 
-options(ggrepel.max.overlaps = Inf)     #*should prevent warning about too many unlabeled data points, but it's not working..
+options(ggrepel.max.overlaps = Inf)     #***should prevent warning about too many unlabeled data points, but it's not working..
 
 ## cnetplot details the genes associated with one or more terms - by default gives the top 5 significant terms (by padj)
 cnetplot(ego, 
@@ -128,7 +133,8 @@ cnetplot(ego,
          color.params = list(foldChange=OE_foldchanges), 
          vertex.label.font=1,
          ggrepel.max.overlaps = Inf)
-#*only shows some labels. has to do with warning? tried increasing max.overlaps, changing label size
+
+#***only shows some labels. has to do with warning? tried increasing max.overlaps, changing label size..
 
 dev.off()                     #close pdf file
 
@@ -136,9 +142,16 @@ dev.off()                     #close pdf file
 #---------------------------
 #Gene Set Enrichment Analysis (FCS)
 
+#using log2fold vals to measure change
+
 ## Remove NA values 
 res_entrez <- dplyr::filter(res_ids, entrezid != "NA")
-## Remove Entrez duplicates [use Entrez IDs this time?]
+## Remove Entrez duplicates
 res_entrez <- res_entrez[which(duplicated(res_entrez$entrezid) == F), ]
-## Sort fold changes in decreasing order  [why?*]
+## Extract the foldchanges
+foldchanges <- res_entrez$log2FoldChange
+## Name each fold change with the corresponding Entrez ID
+names(foldchanges) <- res_entrez$entrezid
+## Sort fold changes in decreasing order
 foldchanges <- sort(foldchanges, decreasing = TRUE)
+
