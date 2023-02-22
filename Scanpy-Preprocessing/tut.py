5f5be99a7b7526ea4511e6a823669b40227ea939
Arfaa Rashid <arfaamrashid@gmail.com>
2023-02-21 20:53:25 -0500

    Initial Commit
    Tutorial on using scanpy to preprocess data for ML
    
    - basic setup of anaconda and input files

---
+++ Scanpy-Preprocessing/tut.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+SCANPY Preprocessing
+Arfaa Rashid
+February 19, 2023
+
+Following tutorial to learn to preprocess data using Python
+
+TUTORIAL: https://scanpy-tutorials.readthedocs.io/en/latest/pbmc3k.html
+INPUT DATA: http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
+
+"""
+
+print("hello, world!")
